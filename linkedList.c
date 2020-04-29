ba05fe54fdd7fa41f3d4a770fb326de155e0de03
xiaoClassamte <s1410643030@gms.nutc.edu.tw>
2020-04-29 14:36:54 +0800

    linked list

--- linkedList.c
+++ linkedList.c
@@ -45,3 +45,40 @@ else{
 }
 
 // 在 Linked List 中找尋特定資料 x 的節點，並將新資料 y 插入在前面
+r = NULL;
+q = head;
+while(q != NULL && q -> data != x){
+	r = q;
+	q = q -> link;
+}
+if (q == NULL){
+	printf("node not foun\n");
+}
+else{
+	p = (NodePtr)malloc(sizeof(struct listnode));
+	p -> data = y;
+	if(r == NULL){
+		head = p;
+	}
+	else{
+		r -> link = p;
+	}
+	p -> link = q;
+}
+
+// 在由小到大的排序中加入一筆資料 x 後仍維持排序
+p = (NodePtr)malloc(sizeof(strcut listnode));
+p -> data = x;
+q = head;
+r = NULL;
+while(q != NULL && x >= q -> data){
+	r = q;
+	q = q -> link;
+}
+if(r == NULL){
+	head = p;
+}
+else{
+	r -> link = p;
+}
+p -> link = q;
